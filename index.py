70047907fb7c0a34550f4029bf53b5da45a09853
Aladser <Aladser@users.noreply.github.com>
2025-01-08 01:07:00 +0900

    форматирование вывода списка игр

--- index.py
+++ index.py
@@ -1,23 +1,63 @@
+import sys
+from typing import Union
+
 from bs4 import BeautifulSoup
 
-with open('./site/index.html', 'r', encoding='utf-8') as file:
-    html_content = file.read()
-soup = BeautifulSoup(html_content, 'html.parser')
+def get_games_info(file_path: str)-> Union[list, None]:
+    """Возвращает информацию об играх"""
 
-games_list = []
-elements = soup.find_all('tr', class_='app')
-for el in elements:
-    td_list = el.find_all('td')
-    image_src = td_list[1].find('img')['src']
-    image_src_list = image_src.split('/')
-    image_src = f"./site/{image_src_list[2]}"
-    game = {
-        'name':td_list[2].text.strip(),
-        'price': td_list[4].text.strip(),
-        'time': td_list[5].text.strip(),
-        'rating': td_list[6].text.strip(),
-        'image': image_src
-    }
-    games_list.append(game)
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            html_content = file.read()
+        soup = BeautifulSoup(html_content, 'html.parser')
+    except FileNotFoundError:
+        print(f"Ошибка: Файл {file_path} не существует", file=sys.stderr)
+        return None
 
-[print(game) for game in games_list]
+    games_list = []
+    elements = soup.find_all('tr', class_='app')
+    for el in elements:
+        td_list = el.find_all('td')
+        # изображение
+        image_src = td_list[1].find('img')['src']
+        image_src_list = image_src.split('/')
+        image_src = f"./site/{image_src_list[2]}"
+        # время
+        time = td_list[5].text.strip()
+        if 'm' in time:
+            time = time.replace('m', '')
+            time = round(int(time) / 60, 2)
+        elif time == '-':
+            time = 0
+        else:
+            time = float(time.replace('h', ''))
+        # цена
+        price = td_list[4].text.strip().replace('No Price', '')
+        if 'Not in Store' in price:
+            price = ''
+        # рейтинг
+        rating = td_list[6].text.strip().replace('-', '')
+
+        game = {
+            'name': td_list[2].text.strip(),
+            'price': price,
+            'time': time,
+            'rating': rating,
+            'image': image_src
+        }
+        games_list.append(game)
+
+    return games_list
+
+games_list = get_games_info('./site/index.html')
+if games_list is None:
+    sys.exit()
+
+for game in games_list:
+    game_info = f"{game['name']}: время {game['time']}ч, "
+    if game['price'] != '':
+        game_info += f"цена {game['price']}, "
+    if game['rating'] != '':
+        game_info += f"рейтинг {game['rating']}, "
+    game_info += f"лого  \"{game['image']}\""
+    print(game_info)
