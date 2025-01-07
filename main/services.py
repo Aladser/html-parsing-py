fa27a0fd7acbc30eca005e6d4316022e71490f67
Aladser <Aladser@users.noreply.github.com>
2025-01-08 08:17:16 +0900

    обновлено получение списка игр

--- main/services.py
+++ main/services.py
@@ -1,54 +1,76 @@
 import sys
-from typing import Union
+import requests
 
-from bs4 import BeautifulSoup
+def get_steam_userid(login, api_key):
+    """Возвращает ID пользователя стим"""
+    url = f'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/'
+    params = {
+        'key': api_key,
+        'vanityurl': login
+    }
 
-from config.settings import MEDIA_ROOT
-
-
-def get_games_info(file_path: str)-> Union[list, None]:
-    """Возвращает информацию об играх"""
-
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            html_content = file.read()
-        soup = BeautifulSoup(html_content, 'html.parser')
-    except FileNotFoundError:
-        print(f"Ошибка: Файл {file_path} не существует", file=sys.stderr)
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('response', {}).get('steamid')
+    else:
+        print(f'Ошибка: {response.status_code}', file=sys.stderr)
         return None
 
-    games_list = []
-    elements = soup.find_all('tr', class_='app')
-    for el in elements:
-        td_list = el.find_all('td')
-        # изображение
-        image_src = td_list[1].find('img')['src']
-        image_src_list = image_src.split('/')
-        image_src = f"/media/site/index/{image_src_list[2]}"
-        # время
-        time = td_list[5].text.strip()
-        if 'm' in time:
-            time = time.replace('m', '')
-            time = round(int(time) / 60, 2)
-        elif time == '-':
-            time = 0
+def get_game_info(appid):
+    """Возвращает информацию об игре"""
+    url = f'https://store.steampowered.com/api/appdetails?appids={appid}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data[str(appid)]['success']:
+            gameinfo = data[str(appid)]['data']
+            # 'metacritic': gameinfo['metacritic'] if 'metacritic' in gameinfo else '',
+            return {
+                'type':gameinfo['type'],
+                'name': gameinfo['name'],
+                'short_description': gameinfo['short_description'],
+                'image': gameinfo['capsule_image'],
+                'background': gameinfo['background'],
+                'release_date': gameinfo['release_date']['date'],
+                'developers': ', '.join(gameinfo['developers']),
+                'publishers': ', '.join(gameinfo['publishers']),
+                'genres': ', '.join([genre['description'] for genre in gameinfo['genres']]),
+                'categories': ', '.join([genre['description'] for genre in gameinfo['categories']]),
+            }
         else:
-            time = float(time.replace('h', ''))
-        # цена
-        price = td_list[4].text.strip().replace('No Price', '')
-        if 'Not in Store' in price:
-            price = ''
-        # рейтинг
-        rating = td_list[6].text.strip().replace('-', '')
+            return None
+    else:
+        print(f'Ошибка: {response.status_code}', file=sys.stderr)
+        return None
 
-        game = {
-            'appid': el['data-appid'],
-            'name': td_list[2].text.strip(),
-            'price': price,
-            'time': time,
-            'rating': rating,
-            'image': image_src
-        }
-        games_list.append(game)
+def get_steam_games_list(userid, apikey):
+    url = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/'
+    params = {
+        'key': apikey,
+        'steamid': userid,
+        'include_appinfo': True,
+        'include_played_free_games': True,
+        'format': 'json'
+    }
 
-    return games_list
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        games_data = response.json()
+        owned_games = games_data.get('response', {}).get('games', [])
+        games_list = []
+
+        if owned_games:
+            for game in owned_games:
+                games_list.append({
+                    'title': game['name'],
+                    'appid': game['appid'],
+                    'time': round(game['playtime_forever']/60, 2),
+                })
+            sorted_games_list = sorted(games_list, key=lambda x: x.get('time', 0), reverse=True)
+            return sorted_games_list
+        else:
+            return None
+    else:
+        print(f"Ошибка: {response.status_code}", file=sys.stderr)
+        return None
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
+import requests
 from django.views.generic import TemplateView
-from config.settings import MEDIA_ROOT
-from main.services import get_games_info
+
+from config.settings import STEAM_USER_ID, STEAM_API_KEY
+from main.services import get_steam_games_list
 
 page = '/site/index.html'
 
@@ -11,5 +13,7 @@ class MainView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['games'] = get_games_info(str(MEDIA_ROOT) + page)
+        games_list = get_steam_games_list(STEAM_USER_ID, STEAM_API_KEY)
+        context['games'] = games_list
+
         return context
