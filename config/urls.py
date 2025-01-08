f250b5c5434c6108d7671b22b3af9aa8a9afe92c
Aladser <Aladser@users.noreply.github.com>
2025-01-08 18:30:49 +0900

    добавлена страница деталей игры

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from main.views import MainView
+from main.views import MainView, GameView
 
 urlpatterns = [
-    path('', MainView.as_view(), name="index")
+    path('', MainView.as_view(), name="index"),
+    path('game/<int:id>', GameView.as_view(), name="game"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,19 +1,43 @@
 from django.views.generic import TemplateView
-
+from steam_web_api import Steam
 from config.settings import STEAM_USER_ID, STEAM_API_KEY
-from main.services import get_steam_games_list
+from main.services import get_steam_games_list, get_game_info
 
-page = '/site/index.html'
 
 class MainView(TemplateView):
-    """Представление главной страницы"""
+    """Главная страница"""
 
     template_name = 'index.html'
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         games_list = get_steam_games_list(STEAM_USER_ID, STEAM_API_KEY)
         context['games'] = games_list
+
+        return context
+
+class GameView(TemplateView):
+    """Страница игры"""
+
+    template_name = 'game.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        steam = Steam(STEAM_API_KEY)
+
+        request_list = self.request.path.split('/')
+        gameid = request_list[-1]
+
+        info = get_game_info(gameid)
+        if info:
+            context['info'] = info
+            context['developers'] = ','.join(info['developers'])
+            context['publishers'] = ','.join(info['publishers'])
+        else:
+            details = steam.apps.get_app_details(gameid)
+            context['info'] = details[gameid]['data'] if details[gameid]['success'] else None
+            """
+            for k,v in details[gameid]['data'].items():
+                print(f"{k} - {v}")
+            """
 
         return context
