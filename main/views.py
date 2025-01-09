e68fe35084a1532588addb91ea37b04058aafdd5
Aladser <Aladser@users.noreply.github.com>
2025-01-10 08:09:15 +0900

    добавлено приложение game

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 from steam_web_api import Steam
-from config.settings import STEAM_USER_ID, STEAM_API_KEY
+
+from config.settings import STEAM_API_KEY
 from main.services import get_steam_games_list, get_game_info, get_steam_userid
 
 
