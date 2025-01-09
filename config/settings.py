33c40414c5bbaa79c50fe43e88164c3a8f0b7a22
Aladser <Aladser@users.noreply.github.com>
2025-01-09 22:10:43 +0900

    обновлен фоновый цвет

--- config/settings.py
+++ config/settings.py
@@ -92,9 +92,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# --- СТАТИКА ---
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
