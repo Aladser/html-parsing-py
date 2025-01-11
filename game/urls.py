ff72709dc74e57f1375bab123b6e698a25510f35
Aladser <Aladser@users.noreply.github.com>
2025-01-11 14:48:05 +0900

    обновлена миграция game

--- game/urls.py
+++ game/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from game.apps import GameConfig
-from game.views import GameListView, GameView
+from game.views import GameListView, GameView, GameDetailView
 
 app_name = GameConfig.name
 
 urlpatterns = [
     path('', GameListView.as_view()),
-    path('detail/<str:id>', GameView.as_view())
+    path('detail/<str:id>', GameView.as_view()),
+    path('game/detail/<str:pk>', GameDetailView.as_view())
 ]
