b919382036a2459d33dd947487432bb25fb8cbe7
Aladser <Aladser@users.noreply.github.com>
2025-01-26 08:58:57 +0900

    добавлена очистка поля поиска игры

--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-// список игр: название=>DOM игры
+// список игр: название=>DOM
 let games_list = {};
 document.querySelectorAll('.game-name').forEach(game => {
     games_list[game.textContent] = game;
@@ -10,12 +10,11 @@ if(search_input) {
     search_input.oninput = () => {
         let search_word = search_input.value.toLowerCase();
         if(search_word == '') {
-            // пустое поле
-            for(let key in games_list) {
-                games_list[key].closest('tr').classList.remove('d-none');
-            }
+            showAllGames();
+            clear_search_btn.classList.add('d-none');
         } else {
             // фраза
+            clear_search_btn.classList.remove('d-none');
             for(let key in games_list) {
                 if(!key.toLowerCase().includes(search_word)) {
                     games_list[key].closest('tr').classList.add('d-none');
@@ -23,4 +22,19 @@ if(search_input) {
             }
         }
     };    
+}
+
+// очистка поля поиска
+let clear_search_btn = document.querySelector('#clear-search-btn');
+if(clear_search_btn) {
+    clear_search_btn.onclick = () => {
+        search_input.value = "";
+        showAllGames()
+    }
+}
+
+function showAllGames() {
+    for(let key in games_list) {
+        games_list[key].closest('tr').classList.remove('d-none');
+    }
 }
