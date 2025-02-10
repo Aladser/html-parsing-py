f9c00adf40e8526c9f3a2a544b39c19535167575
Aladser <Aladser@users.noreply.github.com>
2025-02-10 19:49:07 +0900

    добавлен select числа показываемых игр, кнопка наверх

--- static/index.js
+++ static/index.js
@@ -1,8 +1,7 @@
 // список игр: название=>DOM
-let games_list = {};
-document.querySelectorAll('.game-name').forEach(game => {
-    games_list[game.textContent] = game;
-});
+let game_names_list = {};
+document.querySelectorAll('.game-name').forEach(game => game_names_list[game.textContent] = game);
+let games_list = Array.from(document.querySelectorAll('.game'));
 
 // поиск игр
 let search_input = document.querySelector('#search-input');
@@ -15,9 +14,9 @@ if(search_input) {
         } else {
             // фраза
             clear_search_btn.classList.remove('d-none');
-            for(let key in games_list) {
+            for(let key in game_names_list) {
                 if(!key.toLowerCase().includes(search_word)) {
-                    games_list[key].closest('tr').classList.add('d-none');
+                    game_names_list[key].closest('tr').classList.add('d-none');
                 }
             }
         }
@@ -33,8 +32,26 @@ if(clear_search_btn) {
     }
 }
 
+// выбор числа показываемых игр
+document.querySelector('#game-count-select').onchange = e => {
+    let selected_count = e.target.options[e.target.selectedIndex].value == 'all' ? games_list.length : e.target.options[e.target.selectedIndex].value;
+    let games_count = selected_count > games_list.length ? games_list.length : selected_count;
+    hideAllGames()
+    for (let i=0; i<games_count; i++) {
+        games_list[i].classList.remove('d-none');
+    }
+}
+
+// кнопка наверх
+document.querySelector('#scroll-to-btn').onclick = e => window.scrollTo(0, 0);
+
 function showAllGames() {
     for(let key in games_list) {
-        games_list[key].closest('tr').classList.remove('d-none');
+        games_list[key].classList.remove('d-none');
+    }
+}
+function hideAllGames() {
+    for(let key in games_list) {
+        games_list[key].classList.add('d-none');
     }
 }
