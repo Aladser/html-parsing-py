18ce38f25d0724745eeb7e78930d74252cbeec71
Aladser <Aladser@users.noreply.github.com>
2025-02-10 21:57:57 +0900

    доработан поиск пользователя по id

--- game/views.py
+++ game/views.py
@@ -15,55 +15,62 @@ class GameListView(ListView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
+
         context = super().get_context_data(**kwargs)
         login = context['login'] = self.request.GET['login'] if 'login' in self.request.GET else None
         if login:
             user_request = SteamService.get_steam_userid(login, STEAM_API_KEY)
+
             if user_request['response']:
-                userid = user_request['data']
+                # 76561198071620586
+                # проверка, если введен id пользователя вместо логина
+                userid = user_request['data'] if user_request['data'] else login
                 games_list = SteamService.get_steam_games_list(userid, STEAM_API_KEY)
                 context['games'] = games_list
                 context['userid'] = userid
 
-                context['games_count'] = len(games_list)
-                total_time = sum([game['time'] for game in games_list])
-                context['avg_time'] = round(total_time / context['games_count'], 2)
+                if games_list:
+                    context['games_count'] = len(games_list)
+                    total_time = sum([game['time'] for game in games_list])
+                    context['avg_time'] = round(total_time / context['games_count'], 2)
 
-                game_time_stat = {
-                    '250 часов': {'count': 0, 'percent': 0},
-                    '100 часов': {'count': 0, 'percent': 0},
-                    '50 часов': {'count': 0, 'percent': 0},
-                    '20 часов': {'count': 0, 'percent': 0},
-                    '12 часов': {'count': 0, 'percent': 0},
-                    'менее 12 часов': {'count': 0, 'percent': 0},
-                    '0 часов': {'count': 0, 'percent': 0}
-                }
-                for game in games_list:
-                    if game['time'] >= 250:
-                        game_time_stat['250 часов']['count'] += 1
-                    if game['time'] >= 100:
-                        game_time_stat['100 часов']['count'] += 1
-                    if game['time'] >= 50:
-                        game_time_stat['50 часов']['count'] += 1
-                    if game['time'] >= 20:
-                        game_time_stat['20 часов']['count'] += 1
-                    if game['time'] >= 12:
-                        game_time_stat['12 часов']['count'] += 1
-                    elif game['time'] < 12 and game['time'] != 0:
-                        game_time_stat['менее 12 часов']['count'] += 1
-                    else:
-                        game_time_stat['0 часов']['count'] += 1
+                    game_time_stat = {
+                        '250 часов': {'count': 0, 'percent': 0},
+                        '100 часов': {'count': 0, 'percent': 0},
+                        '50 часов': {'count': 0, 'percent': 0},
+                        '20 часов': {'count': 0, 'percent': 0},
+                        '12 часов': {'count': 0, 'percent': 0},
+                        'менее 12 часов': {'count': 0, 'percent': 0},
+                        '0 часов': {'count': 0, 'percent': 0}
+                    }
+                    for game in games_list:
+                        if game['time'] >= 250:
+                            game_time_stat['250 часов']['count'] += 1
+                        if game['time'] >= 100:
+                            game_time_stat['100 часов']['count'] += 1
+                        if game['time'] >= 50:
+                            game_time_stat['50 часов']['count'] += 1
+                        if game['time'] >= 20:
+                            game_time_stat['20 часов']['count'] += 1
+                        if game['time'] >= 12:
+                            game_time_stat['12 часов']['count'] += 1
+                        elif game['time'] < 12 and game['time'] != 0:
+                            game_time_stat['менее 12 часов']['count'] += 1
+                        else:
+                            game_time_stat['0 часов']['count'] += 1
 
-                context['played_games_count'] = context['games_count'] - game_time_stat['0 часов']['count']
-                for stat in game_time_stat:
-                    game_time_stat[stat]['percent'] = round(
-                        game_time_stat[stat]['count'] * 100 / context['played_games_count'])
-                context['played_games_percent'] = floor(100 * context['played_games_count'] / context['games_count'])
-                context['game_time_stat'] = game_time_stat
-
-                return context
+                    context['played_games_count'] = context['games_count'] - game_time_stat['0 часов']['count']
+                    for stat in game_time_stat:
+                        game_time_stat[stat]['percent'] = round(
+                            game_time_stat[stat]['count'] * 100 / context['played_games_count'])
+                    context['played_games_percent'] = floor(100 * context['played_games_count'] / context['games_count'])
+                    context['game_time_stat'] = game_time_stat
+                    return context
+                else:
+                    context['error'] = "Пользователь не найден, либо игровые данные недоступны"
             else:
                 context['error'] = user_request['data']
+
         context['games'] = None
         return context
 
